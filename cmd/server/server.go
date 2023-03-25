7ad29b8ab397c7b77692c75be222f3e01d679092
Devano Fahrurossi <devano485@gmail.com>
2023-03-25 22:56:20 +0700

    update server

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,9 +1,13 @@
 package server
 
-import "github.com/Krypt9x/blog-backend/internal/api/routes"
+import (
+	"github.com/Krypt9x/blog-backend/internal/api/routes"
+	"github.com/gofiber/fiber/v2/middleware/pprof"
+)
 
 func RunServer() {
 	route := routes.MainRoute{}
 	app := route.NewRouter()
 	app.Listen(":3000")
+	app.Use(pprof.New())
 }
