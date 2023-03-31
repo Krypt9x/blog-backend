97e8958c9b9d029d1b40aa58827ce57a65f69c9b
Devano Fahrurossi <devano485@gmail.com>
2023-03-31 21:53:57 +0700

    add comment controller

--- internal/api/controller/comment_controller.go
+++ internal/api/controller/comment_controller.go
@@ -1,1 +1,9 @@
 package controller
+
+import "github.com/gofiber/fiber/v2"
+
+type CommentController interface {
+	Create(ctx *fiber.Ctx) error
+	Update(ctx *fiber.Ctx) error
+	Delete(ctx *fiber.Ctx) error
+}
--- internal/api/controller/comment_controller_impl.go
+++ internal/api/controller/comment_controller_impl.go
@@ -1,1 +1,71 @@
 package controller
+
+import (
+	services "github.com/Krypt9x/blog-backend/internal/api/service"
+	"github.com/Krypt9x/blog-backend/internal/model/domain"
+	"github.com/Krypt9x/blog-backend/pkg/helper"
+	"github.com/gofiber/fiber/v2"
+)
+
+type CommentControllerImpl struct {
+	CommentService services.CommentService
+}
+
+func (service *CommentControllerImpl) Create(ctx *fiber.Ctx) error {
+	var dataReq domain.Comments
+	if err := ctx.BodyParser(dataReq); err != nil {
+		helper.PanicIfError(err)
+	}
+
+	if err := service.CommentService.Create(ctx.Context(), dataReq); err != nil {
+		return helper.SendJSONResponse(ctx, &helper.HttpResponseData{
+			Code:   fiber.StatusInternalServerError,
+			Status: "failed create",
+			Data:   "internal server error",
+		})
+	}
+
+	return helper.SendJSONResponse(ctx, &helper.HttpResponseData{
+		Code:   fiber.StatusOK,
+		Status: "OK",
+		Data:   "success create comment",
+	})
+
+}
+
+func (service *CommentControllerImpl) Update(ctx *fiber.Ctx) error {
+	var dataReq domain.Comments
+	if err := ctx.BodyParser(dataReq); err != nil {
+		helper.PanicIfError(err)
+	}
+
+	if err := service.CommentService.UpdateById(ctx.Context(), dataReq); err != nil {
+		return helper.SendJSONResponse(ctx, &helper.HttpResponseData{
+			Code:   fiber.StatusInternalServerError,
+			Status: "failed update comment",
+			Data:   "internal server error",
+		})
+	}
+
+	return helper.SendJSONResponse(ctx, &helper.HttpResponseData{
+		Code:   fiber.StatusOK,
+		Status: "OK",
+		Data:   "success update comment",
+	})
+}
+
+func (service *CommentControllerImpl) Delete(ctx *fiber.Ctx) error {
+	if err := service.CommentService.Delete(ctx.Context(), ctx.Params("id")); err != nil {
+		return helper.SendJSONResponse(ctx, &helper.HttpResponseData{
+			Code:   fiber.StatusInternalServerError,
+			Status: "failed delete comment",
+			Data:   "internal server error",
+		})
+	}
+
+	return helper.SendJSONResponse(ctx, &helper.HttpResponseData{
+		Code:   fiber.StatusOK,
+		Status: "OK",
+		Data:   "success delete comment",
+	})
+}
